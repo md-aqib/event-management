7097d554df60d66636b2ef909ec0ffea1a001699
Mohd Aqib <aqib@logicwind.com>
2020-10-07 11:45:47 +0530

    Event model added

---
+++ migrations/20201007061519-create-event.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      eventName: {
+        type: Sequelize.STRING
+      },
+      eventDetails: {
+        type: Sequelize.STRING
+      },
+      createdBy: {
+        type: Sequelize.STRING
+      },
+      invited: {
+        type: Sequelize.ARRAY
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Events');
+  }
+};
---
+++ models/event.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Event extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Event.init({
+    eventName: DataTypes.STRING,
+    eventDetails: DataTypes.STRING,
+    createdBy: DataTypes.STRING,
+    invited: DataTypes.ARRAY,
+    date: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Event',
+  });
+  return Event;
+};
