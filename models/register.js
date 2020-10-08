09c3b6f1ed3d20211c18053ba9f3930bd7ae97df
Mohd Aqib <aqib@logicwind.com>
2020-10-08 19:24:08 +0530

    added associations in models

--- models/register.js
+++ models/register.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Register.belongsToMany(models.Event);
     }
   }
   Register.init(
--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,8 +1,8 @@
-async function feed(parent, args, context, info) {
+async function allEvents(parent, args, context, info) {
   const events = await context.models.Event.findAll();
   return events;
 }
 
 module.exports = {
-  feed,
+  allEvents,
 };
