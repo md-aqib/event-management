7e32ab7a7b0b0d7b811d7bc135560bede0fcc96e
Mohd Aqib <aqib@logicwind.com>
2020-10-08 12:17:01 +0530

    nodemailer.js added

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const mailer = require("../nodemailer");
 const { APP_SECRET, getUserId } = require("../utils");
 
 //register
