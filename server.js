264f18a5a4c39b13c318b01993aaa034a2b58ab5
Mohd Aqib <aqib@logicwind.com>
2020-10-12 19:00:11 +0530

    comment deleted

--- server.js
+++ server.js
@@ -16,18 +16,12 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => {
-    // const token = req.headers.authorization;
-    // console.log('LLLLLLLLLLLLLL',token)
-    // const user = getUser(token);
     return {
       req,
       models,
     };
   },
 });
-
-// models.sequelize.authenticate();
-// models.sequelize.sync();
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
