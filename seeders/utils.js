571b8ff80d4326705066f92d9551481fe4bf7ee6
Mohd Aqib <aqib@logicwind.com>
2020-10-12 18:00:55 +0530

    final commit

--- seeders/utils.js
+++ seeders/utils.js
@@ -3,10 +3,8 @@ const APP_SECRET = "GraphQL-is-aw3some";
 
 function getUserId(context) {
   const token = context.req.headers.authorization
-  console.log('>>>>>>>>>>>????>>',token)
-  // const Authorization = context.request.get("authorization");
+
   if (token) {
-    // const token = Authorization.replace("Bearer ", "");
     const { userId, email } = jwt.verify(token, APP_SECRET);
     return { userId, email };
   }
